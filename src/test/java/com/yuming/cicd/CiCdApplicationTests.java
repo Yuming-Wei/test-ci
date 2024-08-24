980e70cba632cbf4c2c686452e224f8334242dec
Yuming Wei <wymice@gmail.com>
2024-08-24 15:12:08 -0400

    intial commit

---
+++ src/test/java/com/yuming/cicd/CiCdApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yuming.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Pass");
+	}
+
+}
